4baf7895110469e1fc0ac71d1a9b21cf60259099
shivam23kumar <shivamkumar.dev23@gmail.com>
2022-01-19 06:52:19 +0530

    toggle_js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-
+document.querySelector("#toggle").addEventListener("click", function(){
+    document.getElementById("toggle").classList.toggle("active");
+    document.querySelector("body").classList.toggle("active");
+})
 
 
